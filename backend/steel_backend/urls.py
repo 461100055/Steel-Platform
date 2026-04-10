50fd7817c81e3f58fdffafee0ef960d949ebc343
461100055 <461100055@qu.edu.sa>
2026-04-10 07:15:41 +0300

    Fix admin route on Railway

--- backend/steel_backend/urls.py
+++ backend/steel_backend/urls.py
@@ -2,10 +2,8 @@ from django.contrib import admin
 from django.urls import path, include
 from django.http import HttpResponse
 
-
 def home(request):
     return HttpResponse("Steel Platform backend is running.")
-
 
 urlpatterns = [
     path('admin/', admin.site.urls),
